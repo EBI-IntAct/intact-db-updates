8bbd085314cd65a3a66fc500c09c405c0fa31080
marine.dumousseau <marine.dumousseau@wanadoo.fr>
2011-02-16 13:44:27 +0000

    Updated the globalProteinUpdate

--- src/main/java/uk/ac/ebi/intact/GlobaldProteinUpdate.java
+++ src/main/java/uk/ac/ebi/intact/GlobaldProteinUpdate.java
@@ -5,13 +5,9 @@ import uk.ac.ebi.intact.dbupdate.prot.ProteinUpdateContext;
 import uk.ac.ebi.intact.dbupdate.prot.ProteinUpdateProcessor;
 import uk.ac.ebi.intact.dbupdate.prot.ProteinUpdateProcessorConfig;
 import uk.ac.ebi.intact.dbupdate.prot.report.FileReportHandler;
-import uk.ac.ebi.intact.dbupdate.prot.report.UpdateReportHandler;
-import uk.ac.ebi.intact.model.Protein;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * TODO comment this
@@ -26,12 +22,12 @@ public class GlobaldProteinUpdate {
     public static void main(String [] args){
 
         // three possible arguments
-        //if( args.length != 2 ) {
-            //System.err.println( "Usage: GlobalUpdate <database> <folder>" );
-            //System.exit( 1 );
-        //}
-        final String database = "enzpro";
-        final String filename = "/home/marine/test";
+        if( args.length != 2 ) {
+            System.err.println( "Usage: GlobalUpdate <database> <folder>" );
+            System.exit( 1 );
+        }
+        final String database = args[0];
+        final String filename = args[1];
 
         System.out.println( "folder where are the log files = " + filename );
         System.out.println( "database = " + database );
@@ -50,11 +46,11 @@ public class GlobaldProteinUpdate {
 
             ProteinUpdateProcessor updateProcessor = new ProteinUpdateProcessor();
             System.out.println("Starting the global update");
-            //updateProcessor.updateAll();
+            updateProcessor.updateAll();
             //List<Protein> proteins = updateProcessor.retrieveAndUpdateProteinFromUniprot("Q9XYZ4");
-            List<String> acs = new ArrayList<String>();
-            acs.add("EBI-3044019");
-            updateProcessor.updateByACs(acs);
+            //List<String> acs = new ArrayList<String>();
+            //acs.add("EBI-3044019");
+            //updateProcessor.updateByACs(acs);
 
         } catch (IOException e) {
             System.err.println("The repository " + filename + " cannot be found. We cannot write log files and so we cannot run a global protein update.");
