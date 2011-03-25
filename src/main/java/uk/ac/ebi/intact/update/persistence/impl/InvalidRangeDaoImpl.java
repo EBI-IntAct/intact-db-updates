e4c6bbac917160995afb8f6d6ef9f38c2dd3ffc9
marine.dumousseau <marine.dumousseau@wanadoo.fr>
2011-03-25 09:24:20 +0000

    Used criteria and criterions in InvalidRangeDaoImpl

--- src/main/java/uk/ac/ebi/intact/update/persistence/impl/InvalidRangeDaoImpl.java
+++ src/main/java/uk/ac/ebi/intact/update/persistence/impl/InvalidRangeDaoImpl.java
@@ -1,12 +1,13 @@
 package uk.ac.ebi.intact.update.persistence.impl;
 
+import org.apache.commons.lang.time.DateUtils;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import uk.ac.ebi.intact.update.model.protein.update.events.range.InvalidRange;
 import uk.ac.ebi.intact.update.persistence.InvalidRangeDao;
 
-import javax.persistence.Query;
 import java.util.Date;
 import java.util.List;
 
@@ -22,200 +23,144 @@ import java.util.List;
 @Lazy
 public class InvalidRangeDaoImpl extends UpdatedRangeDaoImpl<InvalidRange> implements InvalidRangeDao {
 
-    private final static String invalidRangeAc = "EBI-2907496";
-    private final static String outOfDateRangeAc = "EBI-3058809";
+    private final static String INVALID_RANGE_AC = "EBI-2907496";
+    private final static String OUT_OF_DATE_RANGE_AC = "EBI-3058809";
 
     @Override
     public List<InvalidRange> getAllInvalidRanges() {
-        final Query query = getEntityManager().createQuery( "select ir from InvalidRange ir join ir.featureAnnotations as fa where fa.topic =:topic" );
-        query.setParameter( "topic", invalidRangeAc);
-
-        return query.getResultList();
+        return getSession().createCriteria(InvalidRange.class).createAlias("featureAnnotations", "a")
+                .add(Restrictions.eq("a.topic", INVALID_RANGE_AC)).list();
     }
 
     @Override
     public List<InvalidRange> getAllOutOfDateRanges() {
-        final Query query = getEntityManager().createQuery( "select ir from InvalidRange ir join ir.featureAnnotations as fa where fa.topic =:topic" );
-        query.setParameter( "topic", outOfDateRangeAc);
-
-        return query.getResultList();
+        return getSession().createCriteria(InvalidRange.class).createAlias("featureAnnotations", "a")
+                .add(Restrictions.eq("a.topic", OUT_OF_DATE_RANGE_AC)).list();
     }
 
     @Override
     public List<InvalidRange> getAllOutOfDateRangesWithoutSequenceVersion() {
-        final Query query = getEntityManager().createQuery( "select ir from InvalidRange ir join ir.featureAnnotations as fa where fa.topic =:topic and ir.sequenceVersion = :version" );
-        query.setParameter( "topic", outOfDateRangeAc);
-        query.setParameter( "version", -1);
-
-        return query.getResultList();
+        return getSession().createCriteria(InvalidRange.class).createAlias("featureAnnotations", "a")
+                .add(Restrictions.eq("a.topic", OUT_OF_DATE_RANGE_AC)).add(Restrictions.eq("sequenceVersion", -1)).list();
     }
 
     @Override
     public List<InvalidRange> getAllOutOfDateRangesWithSequenceVersion() {
-        final Query query = getEntityManager().createQuery( "select ir from InvalidRange ir join ir.featureAnnotations as fa where fa.topic =:topic and ir.sequenceVersion <> :version" );
-        query.setParameter( "topic", outOfDateRangeAc);
-        query.setParameter( "version", -1);
-
-        return query.getResultList();
+        return getSession().createCriteria(InvalidRange.class).createAlias("featureAnnotations", "a")
+                .add(Restrictions.eq("a.topic", OUT_OF_DATE_RANGE_AC)).add(Restrictions.ne("sequenceVersion", -1)).list();
     }
 
     @Override
     public List<InvalidRange> getInvalidRanges(long processId) {
-        final Query query = getEntityManager().createQuery( "select ir from InvalidRange ir join ir.featureAnnotations as fa join ir.parent as p where fa.topic =:topic and p.id = :id" );
-        query.setParameter( "topic", invalidRangeAc);
-        query.setParameter( "id", processId);
-
-        return query.getResultList();
+        return getSession().createCriteria(InvalidRange.class).createAlias("featureAnnotations", "a").createAlias("parent", "p")
+                .add(Restrictions.eq("a.topic", INVALID_RANGE_AC)).add(Restrictions.eq("p.id", processId)).list();
     }
 
     @Override
     public List<InvalidRange> getOutOfDateRanges(long processId) {
-        final Query query = getEntityManager().createQuery( "select ir from InvalidRange ir join ir.featureAnnotations as fa join ir.parent as p where fa.topic =:topic and p.id = :id" );
-        query.setParameter( "topic", outOfDateRangeAc);
-        query.setParameter( "id", processId);
-
-        return query.getResultList();
+        return getSession().createCriteria(InvalidRange.class).createAlias("featureAnnotations", "a").createAlias("parent", "p")
+                .add(Restrictions.eq("a.topic", OUT_OF_DATE_RANGE_AC)).add(Restrictions.eq("p.id", processId)).list();
     }
 
     @Override
     public List<InvalidRange> getOutOfDateRangesWithoutSequenceVersion(long processId) {
-        final Query query = getEntityManager().createQuery( "select ir from InvalidRange ir join ir.featureAnnotations as fa join ir.parent as p where fa.topic =:topic and ir.sequenceVersion = :version and p.id = :id" );
-        query.setParameter( "topic", outOfDateRangeAc);
-        query.setParameter( "version", -1);
-        query.setParameter( "id", processId);
-
-
-        return query.getResultList();
+        return getSession().createCriteria(InvalidRange.class).createAlias("featureAnnotations", "a").createAlias("parent", "p")
+                .add(Restrictions.eq("a.topic", OUT_OF_DATE_RANGE_AC)).add(Restrictions.eq("sequenceVersion", -1))
+                .add(Restrictions.eq("p.id", processId)).list();
     }
 
     @Override
     public List<InvalidRange> getOutOfDateRangesWithSequenceVersion(long processId) {
-        final Query query = getEntityManager().createQuery( "select ir from InvalidRange ir join ir.featureAnnotations as fa join ir.parent as p where fa.topic =:topic and ir.sequenceVersion <> :version and p.id = :id" );
-        query.setParameter( "topic", outOfDateRangeAc);
-        query.setParameter( "version", -1);
-        query.setParameter( "id", processId);
-
-
-        return query.getResultList();
+        return getSession().createCriteria(InvalidRange.class).createAlias("featureAnnotations", "a").createAlias("parent", "p")
+                .add(Restrictions.eq("a.topic", OUT_OF_DATE_RANGE_AC)).add(Restrictions.ne("sequenceVersion", -1))
+                .add(Restrictions.eq("p.id", processId)).list();
     }
 
     @Override
     public List<InvalidRange> getInvalidRanges(Date updateddate) {
-        final Query query = getEntityManager().createQuery( "select ir from InvalidRange ir join ir.featureAnnotations as fa join ir.parent as p where fa.topic =:topic and p.date = :date" );
-        query.setParameter( "topic", invalidRangeAc);
-        query.setParameter( "date", updateddate);
-
-        return query.getResultList();
+        return getSession().createCriteria(InvalidRange.class).createAlias("featureAnnotations", "a").createAlias("parent", "p")
+                .add(Restrictions.eq("a.topic", INVALID_RANGE_AC))
+                .add(Restrictions.lt("p.date", DateUtils.addDays(updateddate, 1)))
+                .add(Restrictions.gt("p.date", DateUtils.addDays(updateddate, -1))).list();
     }
 
     @Override
     public List<InvalidRange> getOutOfDateRanges(Date updateddate) {
-        final Query query = getEntityManager().createQuery( "select ir from InvalidRange ir join ir.featureAnnotations as fa join ir.parent as p where fa.topic =:topic and p.date = :date" );
-        query.setParameter( "topic", outOfDateRangeAc);
-        query.setParameter( "date", updateddate);
-
-        return query.getResultList();
+        return getSession().createCriteria(InvalidRange.class).createAlias("featureAnnotations", "a").createAlias("parent", "p")
+                .add(Restrictions.eq("a.topic", OUT_OF_DATE_RANGE_AC))
+                .add(Restrictions.lt("p.date", DateUtils.addDays(updateddate, 1)))
+                .add(Restrictions.gt("p.date", DateUtils.addDays(updateddate, -1))).list();
     }
 
     @Override
     public List<InvalidRange> getOutOfDateRangesWithoutSequenceVersion(Date updateddate) {
-        final Query query = getEntityManager().createQuery( "select ir from InvalidRange ir join ir.featureAnnotations as fa join ir.parent as p where fa.topic =:topic and ir.sequenceVersion = :version and p.date = :date" );
-        query.setParameter( "topic", outOfDateRangeAc);
-        query.setParameter( "version", -1);
-        query.setParameter( "date", updateddate);
-
-
-        return query.getResultList();
+        return getSession().createCriteria(InvalidRange.class).createAlias("featureAnnotations", "a").createAlias("parent", "p")
+                .add(Restrictions.eq("a.topic", OUT_OF_DATE_RANGE_AC)).add(Restrictions.eq("sequenceVersion", -1))
+                .add(Restrictions.lt("p.date", DateUtils.addDays(updateddate, 1)))
+                .add(Restrictions.gt("p.date", DateUtils.addDays(updateddate, -1))).list();
     }
 
     @Override
     public List<InvalidRange> getOutOfDateRangesWithSequenceVersion(Date updateddate) {
-        final Query query = getEntityManager().createQuery( "select ir from InvalidRange ir join ir.featureAnnotations as fa join ir.parent as p where fa.topic =:topic and ir.sequenceVersion <> :version and p.date = :date" );
-        query.setParameter( "topic", outOfDateRangeAc);
-        query.setParameter( "version", -1);
-        query.setParameter( "date", updateddate);
-
-
-        return query.getResultList();
+        return getSession().createCriteria(InvalidRange.class).createAlias("featureAnnotations", "a").createAlias("parent", "p")
+                .add(Restrictions.eq("a.topic", OUT_OF_DATE_RANGE_AC)).add(Restrictions.ne("sequenceVersion", -1))
+                .add(Restrictions.lt("p.date", DateUtils.addDays(updateddate, 1)))
+                .add(Restrictions.gt("p.date", DateUtils.addDays(updateddate, -1))).list();
     }
 
     @Override
     public List<InvalidRange> getInvalidRangesBefore(Date updateddate) {
-        final Query query = getEntityManager().createQuery( "select ir from InvalidRange ir join ir.featureAnnotations as fa join ir.parent as p where fa.topic =:topic and p.date <= :date" );
-        query.setParameter( "topic", invalidRangeAc);
-        query.setParameter( "date", updateddate);
-
-        return query.getResultList();
+        return getSession().createCriteria(InvalidRange.class).createAlias("featureAnnotations", "a").createAlias("parent", "p")
+                .add(Restrictions.eq("a.topic", INVALID_RANGE_AC))
+                .add(Restrictions.le("p.date", updateddate)).list();
     }
 
     @Override
     public List<InvalidRange> getOutOfDateRangesBefore(Date updateddate) {
-        final Query query = getEntityManager().createQuery( "select ir from InvalidRange ir join ir.featureAnnotations as fa join ir.parent as p where fa.topic =:topic and p.date <= :date" );
-        query.setParameter( "topic", outOfDateRangeAc);
-        query.setParameter( "date", updateddate);
-
-        return query.getResultList();
+        return getSession().createCriteria(InvalidRange.class).createAlias("featureAnnotations", "a").createAlias("parent", "p")
+                .add(Restrictions.eq("a.topic", OUT_OF_DATE_RANGE_AC))
+                .add(Restrictions.le("p.date", updateddate)).list();
     }
 
     @Override
     public List<InvalidRange> getOutOfDateRangesWithoutSequenceVersionBefore(Date updateddate) {
-        final Query query = getEntityManager().createQuery( "select ir from InvalidRange ir join ir.featureAnnotations as fa join ir.parent as p where fa.topic =:topic and ir.sequenceVersion = :version and p.date <= :date" );
-        query.setParameter( "topic", outOfDateRangeAc);
-        query.setParameter( "version", -1);
-        query.setParameter( "date", updateddate);
-
-
-        return query.getResultList();
+        return getSession().createCriteria(InvalidRange.class).createAlias("featureAnnotations", "a").createAlias("parent", "p")
+                .add(Restrictions.eq("a.topic", OUT_OF_DATE_RANGE_AC)).add(Restrictions.eq("sequenceVersion", -1))
+                .add(Restrictions.eq("p.date", updateddate)).add(Restrictions.le("p.date", updateddate)).list();
     }
 
     @Override
     public List<InvalidRange> getOutOfDateRangesWithSequenceVersionBefore(Date updateddate) {
-        final Query query = getEntityManager().createQuery( "select ir from InvalidRange ir join ir.featureAnnotations as fa join ir.parent as p where fa.topic =:topic and ir.sequenceVersion <> :version and p.date <= :date" );
-        query.setParameter( "topic", outOfDateRangeAc);
-        query.setParameter( "version", -1);
-        query.setParameter( "date", updateddate);
-
-
-        return query.getResultList();
+        return getSession().createCriteria(InvalidRange.class).createAlias("featureAnnotations", "a").createAlias("parent", "p")
+                .add(Restrictions.eq("a.topic", OUT_OF_DATE_RANGE_AC)).add(Restrictions.ne("sequenceVersion", -1))
+                .add(Restrictions.le("p.date", updateddate)).add(Restrictions.le("p.date", updateddate)).list();
     }
 
     @Override
     public List<InvalidRange> getInvalidRangesAfter(Date updateddate) {
-        final Query query = getEntityManager().createQuery( "select ir from InvalidRange ir join ir.featureAnnotations as fa join ir.parent as p where fa.topic =:topic and p.date >= :date" );
-        query.setParameter( "topic", invalidRangeAc);
-        query.setParameter( "date", updateddate);
-
-        return query.getResultList();
+        return getSession().createCriteria(InvalidRange.class).createAlias("featureAnnotations", "a").createAlias("parent", "p")
+                .add(Restrictions.eq("a.topic", INVALID_RANGE_AC))
+                .add(Restrictions.ge("p.date", updateddate)).list();
     }
 
     @Override
     public List<InvalidRange> getOutOfDateRangesAfter(Date updateddate) {
-        final Query query = getEntityManager().createQuery( "select ir from InvalidRange ir join ir.featureAnnotations as fa join ir.parent as p where fa.topic =:topic and p.date >= :date" );
-        query.setParameter( "topic", outOfDateRangeAc);
-        query.setParameter( "date", updateddate);
-
-        return query.getResultList();
+        return getSession().createCriteria(InvalidRange.class).createAlias("featureAnnotations", "a").createAlias("parent", "p")
+                .add(Restrictions.eq("a.topic", OUT_OF_DATE_RANGE_AC))
+                .add(Restrictions.ge("p.date", updateddate)).list();
     }
 
     @Override
     public List<InvalidRange> getOutOfDateRangesWithoutSequenceVersionAfter(Date updateddate) {
-        final Query query = getEntityManager().createQuery( "select ir from InvalidRange ir join ir.featureAnnotations as fa join ir.parent as p where fa.topic =:topic and ir.sequenceVersion = :version and p.date >= :date" );
-        query.setParameter( "topic", outOfDateRangeAc);
-        query.setParameter( "version", -1);
-        query.setParameter( "date", updateddate);
-
-
-        return query.getResultList();
+        return getSession().createCriteria(InvalidRange.class).createAlias("featureAnnotations", "a").createAlias("parent", "p")
+                .add(Restrictions.eq("a.topic", OUT_OF_DATE_RANGE_AC)).add(Restrictions.eq("sequenceVersion", -1))
+                .add(Restrictions.eq("p.date", updateddate)).add(Restrictions.ge("p.date", updateddate)).list();
     }
 
     @Override
     public List<InvalidRange> getOutOfDateRangesWithSequenceVersionAfter(Date updateddate) {
-        final Query query = getEntityManager().createQuery( "select ir from InvalidRange ir join ir.featureAnnotations as fa join ir.parent as p where fa.topic =:topic and ir.sequenceVersion <> :version and p.date >= :date" );
-        query.setParameter( "topic", outOfDateRangeAc);
-        query.setParameter( "version", -1);
-        query.setParameter( "date", updateddate);
-
-
-        return query.getResultList();
+        return getSession().createCriteria(InvalidRange.class).createAlias("featureAnnotations", "a").createAlias("parent", "p")
+                .add(Restrictions.eq("a.topic", OUT_OF_DATE_RANGE_AC)).add(Restrictions.ne("sequenceVersion", -1))
+                .add(Restrictions.le("p.date", updateddate)).add(Restrictions.ge("p.date", updateddate)).list();
     }
 }
