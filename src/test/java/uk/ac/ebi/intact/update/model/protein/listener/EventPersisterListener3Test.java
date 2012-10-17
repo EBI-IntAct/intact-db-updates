f2620e79617677c580e7e1fa9820d50f25a1ebcf
mdumousseau <mdumousseau@yahoo.com>
2012-10-17 14:39:57 +0000

    EventPersisterListener2Test : split again big test in several small tests

--- src/test/java/uk/ac/ebi/intact/update/model/protein/listener/EventPersisterListener3Test.java
+++ src/test/java/uk/ac/ebi/intact/update/model/protein/listener/EventPersisterListener3Test.java
@@ -21,12 +21,15 @@ import uk.ac.ebi.intact.update.IntactUpdateContext;
 import uk.ac.ebi.intact.update.model.protein.errors.MultiUniprotIdentities;
 import uk.ac.ebi.intact.update.model.protein.errors.NonExistingProteinTranscript;
 import uk.ac.ebi.intact.update.model.protein.errors.PersistentUpdateErrorFactory;
+import uk.ac.ebi.intact.update.model.protein.events.SequenceIdenticalToTranscriptEvent;
 import uk.ac.ebi.intact.update.model.protein.mapping.factories.PersistentReportsFactory;
 import uk.ac.ebi.intact.update.model.protein.mapping.factories.PersistentResultsFactory;
 import uk.ac.ebi.intact.update.persistence.dao.UpdateDaoFactory;
+import uk.ac.ebi.intact.update.persistence.dao.protein.SequenceIdenticalToTranscriptEventDao;
 
 import java.io.File;
 import java.util.Collection;
+import java.util.List;
 
 /**
  * TODO comment this
@@ -57,6 +60,89 @@ public class EventPersisterListener3Test extends IntactBasicTestCase {
     @After
     public void after() throws Exception {
         processor = null;
+    }
+
+    @Test
+    @Transactional(propagation = Propagation.NEVER)
+    @DirtiesContext
+    public void simulation_same_sequence() throws Exception {
+
+        TransactionStatus status = getDataContext().beginTransaction();
+
+        final File dir = new File("target/simulation");
+        UpdateReportHandler reportHandler = new FileReportHandler(dir);
+        ProteinUpdateProcessorConfig config = ProteinUpdateContext.getInstance().getConfig();
+        config.setReportHandler(reportHandler);
+        config.setGlobalProteinUpdate(true);
+
+        // create the proper persistent factories
+        config.setProteinMappingReportFactory(new PersistentReportsFactory());
+        config.setProteinMappingResultsFactory(new PersistentResultsFactory());
+        config.setErrorFactory(new PersistentUpdateErrorFactory());
+
+        getMockBuilder().createInstitution(CvDatabase.INTACT_MI_REF, CvDatabase.INTACT);
+
+        // one protein for all interactions. This protein has the same sequence as one isoform which is not the canonical sequence
+        Protein simple = getMockBuilder().createProtein("P36872", "simple_protein");
+        simple.getBioSource().setTaxId("7227");
+        simple.setSequence("MAGNGEASWCFSQIKGALDDDVTDADIISCVEFNHDGELLATGDKGGRVVIFQRDPASKA" +
+                "ANPRRGEYNVYSTFQSHEPEFDYLKSLEIEEKINKIRWLQQKNPVHFLLSTNDKTVKLWK" +
+                "VSERDKSFGGYNTKEENGLIRDPQNVTALRVPSVKQIPLLVEASPRRTFANAHTYHINSI" +
+                "SVNSDQETFLSADDLRINLWHLEVVNQSYNIVDIKPTNMEELTEVITAAEFHPTECNVFV" +
+                "YSSSKGTIRLCDMRSAALCDRHSKQFEEPENPTNRSFFSEIISSISDVKLSNSGRYMISR" +
+                "DYLSIKVWDLHMETKPIETYPVHEYLRAKLCSLYENDCIFDKFECCWNGKDSSIMTGSYN" +
+                "NFFRVFDRNSKKDVTLEASRDIIKPKTVLKPRKVCTGGKRKKDEISVDCLDFNKKILHTA" +
+                "WHPEENIIAVAATNNLFIFQDKF") ;
+        getCorePersister().saveOrUpdate(simple);
+
+        // one protein no uniprot update with one interaction
+        Protein noUniProtUpdate = getMockBuilder().createProtein("P12342", "no_uniprot_update");
+        noUniProtUpdate.getXrefs().clear();
+        Annotation noUniprot = getMockBuilder().createAnnotation(null, null, CvTopic.NON_UNIPROT);
+        noUniProtUpdate.addAnnotation(noUniprot);
+        getCorePersister().saveOrUpdate(noUniProtUpdate);
+
+        Interaction interaction_1 = getMockBuilder().createInteraction(simple, noUniProtUpdate);
+        getCorePersister().saveOrUpdate(interaction_1);
+
+        Assert.assertEquals(2, getDaoFactory().getProteinDao().countAll());
+        Assert.assertEquals(1, getDaoFactory().getInteractionDao().countAll());
+        Assert.assertEquals(2, getDaoFactory().getComponentDao().countAll());
+
+        getDataContext().commitTransaction(status);
+
+
+        // try the updater
+        ProteinUpdateProcessor protUpdateProcessor = new ProteinUpdateProcessor();
+        // add the persister listener
+        EventPersisterListener persisterListener = (EventPersisterListener) IntactUpdateContext.getCurrentInstance().getProteinPersisterListener();
+        persisterListener.createUpdateProcess();
+        protUpdateProcessor.addListener(persisterListener);
+        protUpdateProcessor.updateAll();
+
+        TransactionStatus status2 = getDataContext().beginTransaction();
+        Assert.assertEquals(2, getDaoFactory().getProteinDao().countAll());
+        Assert.assertEquals(1, getDaoFactory().getInteractionDao().countAll());
+        Assert.assertEquals(2, getDaoFactory().getComponentDao().countAll());
+        getDataContext().commitTransaction(status2);
+
+        TransactionStatus status3 = IntactUpdateContext.getCurrentInstance().beginTransaction();
+        // checks what has been saved
+        UpdateDaoFactory updateFactory = IntactUpdateContext.getCurrentInstance().getUpdateFactory();
+
+        // 1 protein having same sequence as another of its transcripts
+        SequenceIdenticalToTranscriptEventDao seqIdEvtDao = updateFactory.getSequenceIdenticalToTranscriptEventDao();
+        List<SequenceIdenticalToTranscriptEvent> seqIdEvts = seqIdEvtDao.getAll();
+
+        Assert.assertEquals(1, seqIdEvts.size());
+
+        SequenceIdenticalToTranscriptEvent seqIdEvt = seqIdEvts.iterator().next();
+        Assert.assertEquals(simple.getAc(), seqIdEvt.getProteinAc());
+        Assert.assertEquals("P36872", seqIdEvt.getUniprotAc());
+        Assert.assertEquals("P36872-2", seqIdEvt.getMatchingUniprotTranscript());
+        Assert.assertNull(seqIdEvt.getMessage());
+
+        IntactUpdateContext.getCurrentInstance().commitTransaction(status3);
     }
 
     @Test
