f5986d922fb288478edad1e4a42071ac0b59af8b
maximiliankoch <tschaka1904@gmail.com>
2016-07-14 16:29:46 +0100

    Renamed class & added no-uniprot-update

--- src/main/java/uk/ac/ebi/intact/tools/feature/shortlabel/generator/events/AnnotationFoundEvent.java
+++ src/main/java/uk/ac/ebi/intact/tools/feature/shortlabel/generator/events/AnnotationFoundEvent.java
@@ -4,13 +4,13 @@ package uk.ac.ebi.intact.tools.feature.shortlabel.generator.events;
 /**
  * Created by Maximilian Koch (mkoch@ebi.ac.uk).
  */
-public class FeatureAnnotationFoundEvent {
+public class AnnotationFoundEvent {
     private String featureAc;
     private String interactorAc;
     private AnnotationType type;
     private String message;
 
-    public FeatureAnnotationFoundEvent(String featureAc, String interactorAc, AnnotationType type) {
+    public AnnotationFoundEvent(String featureAc, String interactorAc, AnnotationType type) {
         this.featureAc = (featureAc == null) ? "undefined" : featureAc;
         this.interactorAc = (interactorAc == null) ? "undefined" : interactorAc;
         this.type = type;
@@ -36,7 +36,8 @@ public class FeatureAnnotationFoundEvent {
     //TODO We mix here two different things. FEATURE_WRONG && FEATURE_CORRECTED are annotation descriptions. NO_MUTATION_UPDATE is a annotation name.
     public enum AnnotationType {
         NO_MUTATION_UPDATE("no-mutation-update"),
-        NO_MUTATION_EXPORT("no-muation-export");
+        NO_MUTATION_EXPORT("no-mutation-export"),
+        NO_UNIPROT_UPDATE("no-uniprot-update");
 
         private String message;
 
