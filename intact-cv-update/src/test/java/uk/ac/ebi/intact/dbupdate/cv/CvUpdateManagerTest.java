f3c4b2c4aa185b6796102bf85417d5e80cd9f6dc
jmedinaebi <jmedina@ebi.ac.uk>
2024-08-20 08:58:39 +0100

    Fix failing unit tests

--- intact-cv-update/src/test/java/uk/ac/ebi/intact/dbupdate/cv/CvUpdateManagerTest.java
+++ intact-cv-update/src/test/java/uk/ac/ebi/intact/dbupdate/cv/CvUpdateManagerTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -27,6 +28,7 @@ import java.util.Arrays;
 @ContextConfiguration(locations = {"classpath*:/META-INF/intact.spring.xml",
         "classpath*:/META-INF/standalone/*-standalone.spring.xml",
         "classpath*:/META-INF/beanscv*.spring.xml"})
+@TestPropertySource(locations="classpath:/META-INF/cv-update.properties")
 public class CvUpdateManagerTest extends IntactBasicTestCase{
 
     @Autowired
